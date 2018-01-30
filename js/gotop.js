b8923749ebd1351fc2bdcef58e5d487141874282
Pirates357 <hacking.pirates@gmail.com>
2018-01-30 10:32:45 +0545

    Added go to top button in index page

---
+++ js/gotop.js
@@ -0,0 +1,14 @@
+// Check distance to top and display back-to-top.
+$( window ).scroll( function() {
+	if ( $( this ).scrollTop() > 500 ) {
+		$( '.back-to-top' ).addClass( 'show-back-to-top' );
+	} else {
+		$( '.back-to-top' ).removeClass( 'show-back-to-top' );
+	}
+});
+
+// Click event to scroll to top.
+$( '.back-to-top' ).click( function() {
+	$( 'html, body' ).animate( { scrollTop : 0 }, 800 );
+	return false;
+});
