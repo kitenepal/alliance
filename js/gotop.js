6243883fee22d812d313d0dfc981c6e44cd9cc50
Pirates357 <hacking.pirates@gmail.com>
2018-01-30 11:23:02 +0545

    Added rocket goto top button

--- js/gotop.js
+++ js/gotop.js
@@ -1,14 +1,34 @@
-// Check distance to top and display back-to-top.
-$( window ).scroll( function() {
-	if ( $( this ).scrollTop() > 500 ) {
-		$( '.back-to-top' ).addClass( 'show-back-to-top' );
-	} else {
-		$( '.back-to-top' ).removeClass( 'show-back-to-top' );
-	}
-});
+jQuery(window).scroll(function(){
+    if(jQuery(window).scrollTop()<50){
+        jQuery('#rocketmeluncur').slideUp(500);
+    }else{
+        jQuery('#rocketmeluncur').slideDown(500);
+    }
+    var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
+    var scrolltoprocketmeluncur = $('rocketmeluncur');
+var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
+var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
+var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
+var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
+if (basewrocketmeluncur < 1000) {
+var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
+leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
+scrolltoprocketmeluncur.style.left = ( basewrocketmeluncur + leftrocketmeluncur ) + 'px';
+} else {
+scrolltoprocketmeluncur.style.left = 'auto';
+scrolltoprocketmeluncur.style.right = '10px';
+}
+})
 
-// Click event to scroll to top.
-$( '.back-to-top' ).click( function() {
-	$( 'html, body' ).animate( { scrollTop : 0 }, 800 );
-	return false;
+jQuery('#rocketmeluncur').click(function(){
+    jQuery("html, body").animate({ scrollTop: '0px',display:'none'},{
+            duration: 600,  
+            easing: 'linear'
+        });
+    
+    var self = this;
+    this.className += ' '+"launchrocket";
+    setTimeout(function(){
+      self.className = 'showrocket';
+    },800)
 });
