9684b8f06dc1ef76ada2528d4725b71107b3f738
Pirates357 <hacking.pirates@gmail.com>
2018-02-02 09:57:57 +0545

    Added sticky navbar in all pages

--- js/navbar.js
+++ js/navbar.js
@@ -1383,16 +1383,16 @@
 
 
 
-//Velocity
-// window.onscroll = function() { myFunction() };
+//sticky navbar
+window.onscroll = function() { myFunction() };
 
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
